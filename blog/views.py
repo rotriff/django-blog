3446a5cee825805b62b4f8b3f7be85bcf2a7b87d
shellyguns <talleyranv@gmail.com>
2022-09-27 22:08:36 +0300

    created blog urls views base and index html added templates folder and blog app to settings

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
+
+def starting_page(request):
+    return render(request, "blog/index.html")
+
+def posts(request):
+    pass
+
+def post_detail(request):
+    pass
