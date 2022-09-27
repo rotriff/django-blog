adc318a6fa9a6a6bb9b742be29a8195837cbd0c3
shellyguns <talleyranv@gmail.com>
2022-09-28 02:24:52 +0300

    added posts information, made it change dynamically, added detailed post pages

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,94 @@
-import re
+from datetime import date
 from django.shortcuts import render
+
+all_posts = [
+    {
+        "slug": "first-post",
+        "image": "mountains.jpg",
+        "author": "Shellygun",
+        "date": date(2022, 2, 24),
+        "title": "First Post",
+        "excerpt": "Preview text",
+        "content": 
+        """
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae blanditiis 
+        beatae error consequuntur cum dolor quod maiores unde quisquam, at pariatur veritatis 
+        quam natus quaerat, consequatur laboriosam laudantium fuga debitis.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae blanditiis 
+        beatae error consequuntur cum dolor quod maiores unde quisquam, at pariatur veritatis 
+        quam natus quaerat, consequatur laboriosam laudantium fuga debitis.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae blanditiis 
+        beatae error consequuntur cum dolor quod maiores unde quisquam, at pariatur veritatis 
+        quam natus quaerat, consequatur laboriosam laudantium fuga debitis.
+        """
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Maximilian",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Maximilian",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+    
+]
+
+def get_date(post):
+    return post.get('date')
 
 # Create your views here.
 
+
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
-def post_detail(request):
-    pass
+def post_detail(request,slug):
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post
+    })
