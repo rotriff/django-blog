c639ed3ab28226e35e50136ab730a19a1558c9b3
shellyguns <talleyranv@gmail.com>
2022-10-03 21:26:05 +0300

    added n registered models, migrated, added superuser

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
+from curses.ascii import TAB
+from re import A
 from django.contrib import admin
+from .models import Post, Author, Tag
 
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Tag)
