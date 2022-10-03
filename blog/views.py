e531decb1ad358194d04c1d1644f8d3ad7ae2bca
shellyguns <talleyranv@gmail.com>
2022-10-03 23:38:08 +0300

    updated views to use db added tags and clickable email to post-detail

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,7 @@
-from datetime import date
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 
-all_posts = [
-    
-]
-
-def get_date(post):
-    return post.get('date')
-
 # Create your views here.
-
 
 def starting_page(request):
     latest_posts = Post.objects.all().order_by("-date")[:3]
@@ -19,12 +10,14 @@ def starting_page(request):
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 def post_detail(request,slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": identified_post
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
     })
