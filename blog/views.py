11fab3bfabfe35e31f97539a92dbe09fa6e6ddae
shellyguns <talleyranv@gmail.com>
2022-10-03 22:40:41 +0300

    replaced dummy data with actual db, changed views

--- blog/views.py
+++ blog/views.py
@@ -1,71 +1,8 @@
 from datetime import date
 from django.shortcuts import render
+from .models import Post
 
 all_posts = [
-    {
-        "slug": "first-post",
-        "image": "mountains.jpg",
-        "author": "Shellygun",
-        "date": date(2022, 2, 24),
-        "title": "First Post",
-        "excerpt": "Preview text",
-        "content": 
-        """
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae blanditiis 
-        beatae error consequuntur cum dolor quod maiores unde quisquam, at pariatur veritatis 
-        quam natus quaerat, consequatur laboriosam laudantium fuga debitis.
-
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae blanditiis 
-        beatae error consequuntur cum dolor quod maiores unde quisquam, at pariatur veritatis 
-        quam natus quaerat, consequatur laboriosam laudantium fuga debitis.
-
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae blanditiis 
-        beatae error consequuntur cum dolor quod maiores unde quisquam, at pariatur veritatis 
-        quam natus quaerat, consequatur laboriosam laudantium fuga debitis.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
     
 ]
 
@@ -76,8 +13,7 @@ def get_date(post):
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
