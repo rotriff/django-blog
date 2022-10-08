4321db13b9ebcf9bafe6301a22ca730e292000fa
shellyguns <talleyranv@gmail.com>
2022-10-09 01:53:31 +0300

    added comment presentation on page

--- blog/views.py
+++ blog/views.py
@@ -48,7 +48,8 @@ class SinglePostView(View):
         context = {
         "post": post,
         "post_tags": post.tags.all(),
-        "comment_form": CommentForm() 
+        "comment_form": CommentForm() ,
+        "comments":post.comments.all().order_by("-id")
         }
         return render(request, "blog/post-detail.html", context)
     
@@ -65,7 +66,8 @@ class SinglePostView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form":  comment_form 
+            "comment_form":  comment_form,
+            "comments":post.comments.all().order_by("-id") 
         }
         return render(request, "blog/post-detail.html", context)
 
